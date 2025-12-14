24bdcefc2ff7a76a4dcaaf029d67a29d8e01bea6
Pranitha <pranithareddy160805@gmail.com>
2025-12-14 13:23:03 +0530

    Added the case to check the length of the array size of flights

--- flight-booking-app/src/app/flights/flight-list/flight-list.ts
+++ flight-booking-app/src/app/flights/flight-list/flight-list.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Flights } from '../flights';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-flight-list',
   standalone: true,
@@ -10,16 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './flight-list.html',
   styleUrls: ['./flight-list.css'],
 })
-export class FlightList implements OnInit {
+export class FlightListComponent  implements OnInit {
   flights: any[] = [];
 
-  constructor(private flightService: Flights) {}
+  constructor(private flightService: Flights,private cd: ChangeDetectorRef) {
+  }
 
   ngOnInit(): void {
     this.flightService.getAllFlights().subscribe({
       next: (res) => {
         this.flights = res;
         console.log('Flights loaded:', this.flights);
+        this.cd.detectChanges();
       },
       error: (err) => console.log('Error loading flights', err),
     });
