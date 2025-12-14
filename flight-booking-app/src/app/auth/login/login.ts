c191337d2b9719130084fe8926740084ff865d25
Pranitha <pranithareddy160805@gmail.com>
2025-12-14 20:56:31 +0530

    Modified login component

--- flight-booking-app/src/app/auth/login/login.ts
+++ flight-booking-app/src/app/auth/login/login.ts
@@ -1,54 +1,48 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {Auth} from '../auth';
-import { Router,RouterLink } from '@angular/router';
+import { Auth } from '../auth';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone:true,
+  standalone: true,
   imports: [
-    FormsModule,CommonModule,RouterLink
+    FormsModule, CommonModule, RouterLink
   ],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class Login {
-  username=''
-  password=''
-  errorMessage=''
-  successMessage=''
-  constructor(private auth: Auth, private router: Router) {}
+  username = ''
+  password = ''
+  errorMessage = ''
+  successMessage = ''
+  constructor(private auth: Auth, private router: Router) { }
 
-  login(){
+  login() {
     console.log("login clicked");
-    const body={
-      username:this.username,
-      password:this.password,
-      
+    const body = {
+      username: this.username,
+      password: this.password,
+
     };
-    
-    console.log(body);
     this.auth.login(body).subscribe({
-  next: (res) => {
-    
-
-  
-      const role = this.auth.getUserRole();
-    console.log("Login response:", res);
-    this.auth.saveToken(res.message);
-     if (role === 'ROLE_ADMIN') {
-        this.router.navigate(['/admin']);
-      } else {
-        this.router.navigate(['/flights']);
+      next: (res) => {
+        const role = this.auth.getUserRole();
+        this.auth.saveToken(res.message);
+        if (role === 'ROLE_ADMIN') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/flights']);
+        }
+      },
+      error: (err) => {
+        console.error("Login error:", err);
       }
-    },
-  error: (err) => {
-    console.error("Login error:", err);
-  }
-});
+    });
 
-  
+
   };
-  
+
 }
