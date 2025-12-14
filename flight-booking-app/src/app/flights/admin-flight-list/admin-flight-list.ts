e0f7528d8d0e616767a40c4bdf362e16afc7c21c
Pranitha <pranithareddy160805@gmail.com>
2025-12-14 14:46:59 +0530

    Added change detector

--- flight-booking-app/src/app/flights/admin-flight-list/admin-flight-list.ts
+++ flight-booking-app/src/app/flights/admin-flight-list/admin-flight-list.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Flights } from '../flights';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-flight-list',
-  imports: [],
+  selector: 'app-admin-flights',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-flight-list.html',
-  styleUrl: './admin-flight-list.css',
+  styleUrls: ['./admin-flight-list.css']
 })
-export class AdminFlightList {
+export class AdminFlightList implements OnInit {
 
+  flights: any[] = [];
+
+  constructor(private flightService: Flights, private router: Router,private cd:ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadFlights();
+  }
+
+  loadFlights() {
+    this.flightService.getAllFlights().subscribe(res => {
+      console.log(res);
+      this.flights = res;
+      this.cd.detectChanges(); 
+    });
+  }
+
+  editFlight(id: string) {
+    this.router.navigate(['/admin/flights/edit', id]);
+  }
+
+  deleteFlight(id: string) {
+    if (confirm("Are you sure you want to delete this flight?")) {
+      this.flightService.deleteFlight(id).subscribe(() => {
+        alert("Flight deleted");
+        this.loadFlights();
+      });
+    }
+  }
 }
