79a6bd24bcc14a6779c812585bd56c450dcbb71a
Pranitha <pranithareddy160805@gmail.com>
2025-12-14 21:14:03 +0530

    Removed console.log statements

--- flight-booking-app/src/app/flights/flight-list/flight-list.ts
+++ flight-booking-app/src/app/flights/flight-list/flight-list.ts
@@ -1,27 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Flights } from '../flights';
-// import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-flight-list',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [CommonModule],
   templateUrl: './flight-list.html',
   styleUrls: ['./flight-list.css'],
 })
-export class FlightListComponent  implements OnInit {
+export class FlightListComponent implements OnInit {
   flights: any[] = [];
 
-  constructor(private flightService: Flights,private cd: ChangeDetectorRef) {
-  }
+  constructor(private flightService: Flights, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.flightService.getAllFlights().subscribe({
       next: (res) => {
         this.flights = res;
-        console.log('Flights loaded:', this.flights);
         this.cd.detectChanges();
       },
       error: (err) => console.log('Error loading flights', err),
