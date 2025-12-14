8c5b37d5e9feb8761ad0b3a6dab1d4ec27f39b61
Pranitha <pranithareddy160805@gmail.com>
2025-12-14 17:29:16 +0530

    Modified airline list

--- flight-booking-app/src/app/airlines/admin-airline-list/admin-airline-list.ts
+++ flight-booking-app/src/app/airlines/admin-airline-list/admin-airline-list.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Airlines } from '../airlines'; 
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-admin-airline-list',
-  imports: [],
+  standalone: true,
+    imports: [CommonModule, RouterModule],
   templateUrl: './admin-airline-list.html',
-  styleUrl: './admin-airline-list.css',
+  styleUrls: ['./admin-airline-list.css']
 })
-export class AdminAirlineList {
+export class AdminAirlineList implements OnInit {
 
+  airlines: any[] = [];
+
+  constructor(private airlineService: Airlines, private router: Router,private cd:ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadAirlines();
+  }
+
+  loadAirlines() {
+    this.airlineService.getAllAirlines().subscribe(res => {
+      this.airlines = res;
+      console.log(res);
+      this.cd.detectChanges();
+    });
+  }
+
+  addAirline() {
+    this.router.navigate(['/admin/airlines/add']);
+  }
 }
