ca1c55aec09dde0cd56065916bb20b1cea5528e7
Pranitha <pranithareddy160805@gmail.com>
2025-12-19 21:12:50 +0530

    Modified add-booking

--- src/app/booking/add-booking/add-booking.ts
+++ src/app/booking/add-booking/add-booking.ts
@@ -12,86 +12,10 @@ import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-add-booking',
   standalone:true,
-  imports: [CommonModule, FormsModule,
-    // ActivatedRoute
-  ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-booking.html',
   styleUrl: './add-booking.css',
 })
-// export class AddBooking {
-//   TRIP_TYPE = TRIP_TYPE;
-//   MEAL_PREFERENCE = MEAL_PREFERENCE;
-//   flight: any = null;
-//   bookings: Bookings = {
-//     email: '',
-//     seatCount: 1,
-//     tripType: TRIP_TYPE.ONE_WAY,
-//     mealPreference: MEAL_PREFERENCE.VEG,
-//     flightId: '',
-//     userIds: [],
-//     seatNumbers: []
-//   };
-//   errorMessage = '';
-//   userIdsInput = '';
-//   seatNumbersInput = '';
-
-//   constructor(
-//     private bookingService: Booking,
-//     private router: Router,
-//     private authService: Auth,
-//     private cd: ChangeDetectorRef
-
-//   ) {
-//     const nav = this.router.getCurrentNavigation(); this.flight = nav?.extras?.state?.['flight']; if (this.flight) {
-//       this.bookings.flightId = this.flight.flightId;
-
-//     }
-//   }
-//   ngOnInit() {
-//     this.bookings.email = this.authService.getUserEmail()!;
-//     this.bookings.userIds = [this.authService.getUserId()!];
-
-//   }
-
-//   updateUserIds() {
-//     this.bookings.userIds = this.userIdsInput
-//       .split(',')
-//       .map(v => v.trim())
-//       .filter(v => v.length > 0);
-//   }
-
-//   updateSeatNumbers() {
-//     this.bookings.seatNumbers = this.seatNumbersInput
-//       .split(',')
-//       .map(v => v.trim())
-//       .filter(v => v.length > 0);
-//   }
-
-//   addBooking(form: any) {
-//     if (form.invalid) {
-//       form.control.markAllAsTouched();
-//       return;
-//     }
-//     console.log(this.flight);
-//     this.bookingService.bookTicket(this.flight.id, this.bookings).subscribe({
-//       next: (res:any) => {
-       
-//         this.router.navigate(['/payment'], {
-//           state: {
-//             pnr: res.pnr,
-//             amount: res.totalAmount
-//           }
-//         });
-//       },
-
-//       error: (err) => {
-//         this.errorMessage = err.error?.message || 'Unexpected error occurred';
-//         console.log(this.errorMessage);
-//         this.cd.detectChanges();
-//       }
-//     });
-//   }
-// }
 export class AddBooking {
   TRIP_TYPE = TRIP_TYPE;
   MEAL_PREFERENCE = MEAL_PREFERENCE;
