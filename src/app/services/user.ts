c35a20029fd9a3f52b7119f2fa0a064729b3773f
Pranitha <pranithareddy160805@gmail.com>
2025-12-19 15:50:39 +0530

    Modified the post request from simple Booking to Booking[]

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,20 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface AuthResponse {
+  id: string;
+  email: string;
+  username: string;
+  role: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class User {
-
+export class User{
   private baseUrl = 'http://localhost:8765/user-service/auth';
 
   constructor(private http: HttpClient) {}
 
-  getProfile() {
-    return this.http.get(`${this.baseUrl}/me`);
+  getProfile(): Observable<AuthResponse> {
+    return this.http.get<AuthResponse>(`${this.baseUrl}/me`);
   }
 
-  updateProfile(data: any) {
-    return this.http.put(`${this.baseUrl}/me`, data);
+  updateProfile(data: Partial<AuthResponse>): Observable<AuthResponse> {
+    return this.http.put<AuthResponse>(`${this.baseUrl}/me`, data);
+  }
+
+  getPassengerByEmail(email: string): Observable<AuthResponse> {
+    return this.http.get<AuthResponse>(`${this.baseUrl}/get/email/${encodeURIComponent(email)}`);
+  }
+
+
+  getPassengerById(passengerId: string): Observable<AuthResponse> {
+    return this.http.get<AuthResponse>(`${this.baseUrl}/get/${passengerId}`);
+  }
+
+  deletePassenger(passengerId: string): Observable<string> {
+    return this.http.delete<string>(`${this.baseUrl}/delete/${passengerId}`);
   }
 }
