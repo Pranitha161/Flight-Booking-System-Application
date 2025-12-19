5b1d008b7c974dcf8457969c3262cd176fbb6186
Pranitha <pranithareddy160805@gmail.com>
2025-12-19 21:13:34 +0530

    Added logic and UI for delete-booking feature

--- src/app/booking/delete-booking/delete-booking.ts
+++ src/app/booking/delete-booking/delete-booking.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Booking } from '../booking';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delete-booking',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './delete-booking.html',
   styleUrl: './delete-booking.css',
 })
-export class DeleteBooking {
 
+export class DeleteBooking{
+  pnr='';
+  errorMessage='';
+    constructor(private bookingService:Booking,private router:Router,private cd:ChangeDetectorRef){
+      const nav=this.router.getCurrentNavigation();
+      this.pnr=nav?.extras?.state?.['pnr'];
+      console.log(this.pnr);
+    }
+    cancelBooking(){
+    if(this.pnr){
+    this.bookingService.cancelBooking(this.pnr).subscribe({
+      next:()=>{
+        console.log("deleted")
+        this.router.navigate(['/history']);
+        this.cd.detectChanges();
+
+      },error:err=>{
+        this.errorMessage=err.error?.message||'Cannot delete the booking';
+        this.cd.detectChanges();
+        console.log(err.error);
+        
+      }
+    })
+    }
+  }
+  
+  goback(){
+    this.router.navigate(['/history']);
+  }
+    
 }
