b873aeb24b106a4fcbe60d6f9b804b2f7ffb5e0a
Pranitha <pranithareddy160805@gmail.com>
2025-12-20 11:28:37 +0530

    Added logic to delete only which are having the possiblity to delete the other which are completed will not have the cancel booking button enables

--- src/app/booking/user-bookings/user-bookings.ts
+++ src/app/booking/user-bookings/user-bookings.ts
@@ -4,6 +4,8 @@ import { Booking } from '../booking';
 import { Auth } from '../../auth/auth';
 import { Bookings } from '../booking.module';
 import { Router } from '@angular/router';
+import { Flight } from '../../flights/flight.model';
+import { Flights } from '../../flights/flights';
 
 @Component({
   selector: 'app-user-bookings',
@@ -12,9 +14,9 @@ import { Router } from '@angular/router';
   styleUrl: './user-bookings.css',
 })
 export class UserBookings implements OnInit{
-  bookings: Bookings[] = [];
+  bookings: (Bookings& { flight?: Flight })[] = [];
   errorMessage='';
-  constructor (private bookingService:Booking,private authService :Auth,private cd:ChangeDetectorRef,private router:Router){}
+  constructor (private bookingService:Booking,private authService :Auth,private flightService:Flights,private cd:ChangeDetectorRef,private router:Router){}
   
   ngOnInit() {
     const email=this.authService.getUserEmail()!;
@@ -22,7 +24,12 @@ export class UserBookings implements OnInit{
     this.bookingService.getByEmailId(email).subscribe(
       { 
         next: (res) => {this.bookings = res;
+          console.log(res);
           this.cd.detectChanges();
+          this.bookings.forEach((b) => { 
+            this.flightService.getFlightById(b.flightId).subscribe({ next: (flight: Flight) => { b.flight = flight; this.cd.detectChanges(); }, error: () => { b.flight = undefined; }, }); 
+            console.log(this.bookings);
+          });
         },
          error: (err) => this.errorMessage = err.error?.message || 'Failed to load booking history' 
         });
@@ -31,5 +38,6 @@ export class UserBookings implements OnInit{
     console.log(booking+" "+booking.pnr);
     this.router.navigate(['/delete'],{state:{pnr:booking.pnr}});
   }
+  canCancel(booking: Bookings & { flight?: Flight }): boolean { if (!booking.flight?.departureTime) return false; const departure = new Date(booking.flight.departureTime).getTime(); return departure > Date.now(); }
 
 }
