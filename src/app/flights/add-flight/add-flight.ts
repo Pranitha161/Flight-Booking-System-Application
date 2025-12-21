f915bb740ff8fb7e8cbbcd0f3b7311fc4a4cc039
Pranitha <pranithareddy160805@gmail.com>
2025-12-21 09:53:41 +0530

    Modified the from and to Place from input to dropdown

--- src/app/flights/add-flight/add-flight.ts
+++ src/app/flights/add-flight/add-flight.ts
@@ -27,6 +27,7 @@ export class AddFlight implements OnInit {
     },
     airlineId: ''
   };
+  states: string[] = [];
   errorMessage = '';
   constructor(private flightService: Flights, private router: Router, private cd: ChangeDetectorRef, private route: ActivatedRoute) { }
   ngOnInit(): void {
@@ -35,6 +36,7 @@ export class AddFlight implements OnInit {
         this.flight.airlineId = params['airlineId'];
       }
     });
+    this.states = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal', 'Andaman and Nicobar Islands', 'Chandigarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Jammu and Kashmir', 'Ladakh', 'Lakshadweep', 'Puducherry'];
   }
 
   addFlight(form: any) {
