bedad5d7df1a35f7111b44cd19a2d93e2e4d7f26
Pranitha <pranithareddy160805@gmail.com>
2025-12-21 09:16:12 +0530

    Added the interface of booking with flight as in backend for simplicity the booking only contains flightID

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,32 +1,36 @@
-export enum TRIP_TYPE{
-	ONE_WAY='ONE_WAY',
-	ROUND_TRIP='ROUND_TRIP'
+import { Flight } from '../flights/flight.model';
+export enum TRIP_TYPE {
+	ONE_WAY = 'ONE_WAY',
+	ROUND_TRIP = 'ROUND_TRIP'
 }
-export enum MEAL_PREFERENCE{
-	VEG='VEG',
-	NON_VEG='NON_VEG'
+export enum MEAL_PREFERENCE {
+	VEG = 'VEG',
+	NON_VEG = 'NON_VEG'
 }
-export enum BOOKING_STATUS{
-	
-	CONFIRMED='CONFIRMED',
-	CANCELLED='CANCELLED',
-	COMPLETED='COMPLETED', 
-	EXPIRED='EXPIRED'
+export enum BOOKING_STATUS {
+
+	CONFIRMED = 'CONFIRMED',
+	CANCELLED = 'CANCELLED',
+	COMPLETED = 'COMPLETED',
+	EXPIRED = 'EXPIRED'
 }
-export interface Bookings{
-	id?:string;
-	pnr?:string;
-	email:string;
-	seatCount:number;
-	tripType:TRIP_TYPE;
-	status:BOOKING_STATUS;
-	mealPreference:MEAL_PREFERENCE;
-	flightId:string;
-	userIds:string[];
-	seatNumbers:string[];
-	totalAmount?:number;
+export interface Bookings {
+	id?: string;
+	pnr?: string;
+	email: string;
+	seatCount: number;
+	tripType: TRIP_TYPE;
+	status: BOOKING_STATUS;
+	mealPreference: MEAL_PREFERENCE;
+	flightId: string;
+	userIds: string[];
+	seatNumbers: string[];
+	totalAmount?: number;
 }
 
 
-	
-	
+export interface BookingWithFlight extends Bookings {
+	flight?: Flight;
+}
+
+
