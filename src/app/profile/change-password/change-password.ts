71c50f1ec6c225b7c56f9e8fe3fc13268eaf309d
Pranitha <pranithareddy160805@gmail.com>
2025-12-21 11:51:37 +0530

    Added component for change-password

---
+++ src/app/profile/change-password/change-password.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { User } from '../../services/user';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-change-password',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './change-password.html',
+  styleUrl: './change-password.css',
+})
+export class ChangePassword {
+  oldPassword = '';
+  newPassword = '';
+
+  message: string | null = null;
+  constructor(private userService: User, private router: Router,private cd:ChangeDetectorRef) { }
+  goToProfile() { this.router.navigate(['/profile']); }
+  onSubmit(form: any) {
+    console.log(form.valid);
+    console.log(this.oldPassword+" "+this.newPassword);
+    if ( this.oldPassword != this.newPassword) {
+      this.userService.changePassword(this.oldPassword, this.newPassword).subscribe({
+        next: (res:any) => {
+          this.message = res.message+" "+"Need to login again";
+          localStorage.removeItem('token');
+          localStorage.removeItem('username');
+          this.cd.detectChanges();
+          setTimeout(() => { this.router.navigate(['/login']); }, 1500);
+        },error:(err:any)=>{
+          this.message=err.error.message||"Error changing password";
+          this.cd.detectChanges();
+          
+        }
+      });
+    }
+  }
+}
