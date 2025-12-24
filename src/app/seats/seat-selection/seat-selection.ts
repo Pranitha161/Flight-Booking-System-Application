92c1dd8727556670751d7a0411031c0ebed68201
Pranitha <pranithareddy160805@gmail.com>
2025-12-24 16:19:44 +0530

    Update seat selection UI

--- src/app/seats/seat-selection/seat-selection.ts
+++ src/app/seats/seat-selection/seat-selection.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Seats, Seat } from '../seats'
 import { FormsModule } from '@angular/forms';
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
 export class SeatSelection implements OnInit {
 
   @Input() flightId!: string;
+  @Output() seatSelected = new EventEmitter<Seat>();
 
   seats: Seat[] = [];
   selectedSeat: Seat | null = null;
@@ -23,13 +24,15 @@ export class SeatSelection implements OnInit {
   }
 
   loadSeats() {
-    this.seatService.getSeatsByFlightId(this.flightId).subscribe(data => {
-      this.seats = data;
+    this.seatService.getSeatsByFlightId(this.flightId).subscribe({
+      next:(data)=>this.seats = data,
+      error:(err)=>console.log("failed to load",err)
     });
   }
 
   selectSeat(seat: Seat) {
     if (!seat.available) return;
     this.selectedSeat = seat;
+    this.seatSelected.emit(seat);
   }
 }
